feb04745b8a0f80eb666f326fc4ff5488b15e8a1
saohwan <saohwan@gmail.com>
2024-03-11 21:35:59 +0900

    gitignore update.

---
+++ main.py
@@ -0,0 +1,33 @@
+from typing import Annotated
+
+from fastapi import FastAPI, Depends
+from fastapi import HTTPException
+from sqlalchemy.orm import Session
+from starlette import status
+
+import auth
+import models
+from database import engine, SessionLocal
+
+app = FastAPI()
+app.include_router(auth.router)
+
+models.Base.metadata.create_all(bind=engine)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+
+@app.get("/", status_code=status.HTTP_200_OK)
+async def user(user: None, db: db_dependency):
+    if user is None:
+        raise HTTPException(status_code=401, detail="Authentication Failed")
+    return {"User": user}
